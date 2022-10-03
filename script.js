2083d616a7be63263158a0368a6eb6f21ae51d5b
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-10-03 18:25:28 -0400

    Change input section background to make inputs more visible

---
+++ script.js
@@ -0,0 +1,17 @@
+const pwd = document.querySelector(".pwd");
+const pwdConfirm = document.querySelector(".pwdConfirm");
+const errorMsg= document.querySelector(".error-msg");
+const signupBtn = document.querySelector("#signup");
+
+signupBtn.addEventListener("click", (e) => {
+    checkpassword(pwd.value, pwdConfirm.value)
+})
+    
+
+function checkpassword(p, pc) {
+    if (p !== pc) {
+        errorMsg.textContent = "Passwords must match"
+    } else {
+        errorMsg.textContent = ''
+    };
+}
